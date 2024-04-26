f9fbd722f5379ef58717ed87d8f1c6b5b594026d
Pavlo Zimnytsia <paha20zimnica16@gmail.com>
2024-04-26 03:38:33 +0300

    Initial commit

---
+++ src/main/java/org/example/testtask/config/MySpringMVCDispatcherServletInitializer.java
@@ -0,0 +1,36 @@
+package ua.alishev.springcourse.config;
+
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class}; //Вказуємо файл конфігурації.
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"}; // Еквівалентно перенаправленню на dispatcher у файлі web.xml
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    // Встановили фільтр, щоб спрацювали PATCH/DELETE запити.
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
+    }
+}
